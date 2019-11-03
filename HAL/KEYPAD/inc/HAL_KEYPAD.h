9d255f27351b7e08ff65068ec7db42475d8cebfb
ali225 <aligamal_25@yahoo.com>
2019-11-03 14:29:56 +0200

    Driver Keypad

---
+++ HAL/KEYPAD/inc/HAL_KEYPAD.h
@@ -0,0 +1,18 @@
+/*
+ * HAL_KEYPAD.h
+ *
+ *  Created on: Oct 13, 2019
+ *      Author: Eng Ali Gamal
+ */
+
+#ifndef HAL_KEYPAD_INC_HAL_KEYPAD_H_
+#define HAL_KEYPAD_INC_HAL_KEYPAD_H_
+
+
+
+/************* Function declaration section ************/
+extern stdReturnType_t initKeypad(void);
+extern stdReturnType_t GetPressedKey(u8 *ppressed);
+
+
+#endif /* HAL_KEYPAD_INC_HAL_KEYPAD_H_ */
